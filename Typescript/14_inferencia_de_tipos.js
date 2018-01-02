19d0a8285cd6a6ff83090581c6fe59a30cb2f48c
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-02 21:16:56 -0200

    14 - Inferência de tipos

---
+++ Typescript/14_inferencia_de_tipos.js
@@ -0,0 +1,9 @@
+var mensagem = "Seja bem-vindo!";
+var temporadaFriends = 10;
+var estudoAngular = true;
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+var listaDeFrutas2 = ['Uva', 'Banana', 'Abacaxi']; //não pode repetir o nome
+var notasDasProvas = [7.5, 8, 9];
+var notasDasProvas2 = [7.5, 8, 9];
+//estudoAngular = 'Teste'; //não pode
+//mensagem = 10; não pode
---
+++ Typescript/14_inferencia_de_tipos.ts
@@ -0,0 +1,14 @@
+let mensagem: string  = "Seja bem-vindo!";
+let temporadaFriends:number  = 10;
+let estudoAngular: boolean = true;
+
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi'];
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']; //não pode repetir o nome
+
+let notasDasProvas: Array<number>= [7.5, 8,9];
+let notasDasProvas2: number[]= [7.5, 8,9];
+
+//estudoAngular = 'Teste'; //não pode
+
+//mensagem = 10; não pode
+
